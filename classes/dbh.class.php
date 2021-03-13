152c0c107d17489380c20163e12dd18dd16323fe
zhenchai <zhenchai1111@gmail.com>
2021-03-13 09:11:43 +0800

    Connection to the Database

---
+++ classes/dbh.class.php
@@ -0,0 +1,14 @@
+<?php
+    class Dbh {
+        private $host = "localhost";
+        private $user = "root";
+        private $pwd = "";
+        private $dbName = "oop-php";
+
+        protected function connect() {
+            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname; 
+            $pdo = new PDO($dsn, $this->user, $this->pwd);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $pdo;
+        }
+    }
